7f5ec90ea19e21244e4f438025f4758699f5e4f0
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-07-21 20:21:17 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,106 @@
-def create_an_empty_array
+def 
+  
+  create_an_empty_array
+  
+  Array.new
   
 end
 
-def create_an_array
+
+
+
+def 
+  
+  create_an_array
+  
+  array =
+  [ #"y'all", "said", "some", "data"  >:T lol
+    "wow", "i", "am", "an"
+  ]
+  
   
 end
 
-def add_element_to_end_of_array(array, element)
+
+
+
+def 
+  
+  add_element_to_end_of_array(array, element)
+  
+  array << element
   
 end
 
-def add_element_to_start_of_array(array, element)
+
+
+
+def 
+  
+  add_element_to_start_of_array(array, element)
+  
+  array.unshift(element)
   
 end
 
-def remove_element_from_end_of_array(array)
+
+
+
+def 
+  
+  remove_element_from_end_of_array(array)
+  
+  array.pop
   
 end
 
-def remove_element_from_start_of_array(array)
+
+
+
+def 
+  
+  remove_element_from_start_of_array(array)
+  
+  array.shift
   
 end
 
-def retrieve_element_from_index(array, index_number)
+
+
+
+def 
+  
+  retrieve_element_from_index(array, index_number)
+  
+  puts "#{array[index_number]}"
+  
+  array[index_number]
   
 end
 
-def retrieve_first_element_from_array(array)
+
+
+
+def 
+  
+  retrieve_first_element_from_array(array)
+  
+  puts "#{array[0]}"
+  
+  array[0]
   
 end
 
-def retrieve_last_element_from_array(array)
+
+
+
+def 
+  
+  retrieve_last_element_from_array(array)
+  
+  puts "#{array[(array.size-1)]}"
+  
+  array[(array.size-1)]
   
 end
+
